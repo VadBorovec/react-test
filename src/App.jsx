d4e5d928716e2f4ee26cf650a4f834abe79441bc
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-05-23 22:10:27 +0300

    update 01-2-lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import ProductList from "./components/ProductList";
-import { products } from "./assets/products";
-import Title from "./components/ui/Title";
+import ProductList from './components/ProductList';
+import { products } from './assets/products';
+import Title from './components/ui/Title';
 
 export default function App() {
   return (
@@ -8,7 +8,6 @@ export default function App() {
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <Title title="Customers also purchased" />
         <ProductList data={products} />
-        {/* {productList(products)} */}
       </div>
     </div>
   );
