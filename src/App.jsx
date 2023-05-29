898b0e0de03fc2bd2f4ab7ae3365fbf24efb0512
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-05-29 22:48:54 +0300

    add Counter,DropDown & ColorPicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,164 +1,193 @@
-import React from "react";
-import { TopBlock, Title, MainSection } from "./app.styled";
-import Container from "./components/ui/Container";
-import Cart from "./components/Cart/Cart";
-import Header from "./components/Header/Header";
-import ProductsList from "./components/ProductsList";
-import AuthForm from "./components/Form/AuthForm";
-import Search from "./components/Search";
-import OrderForm from "./components/OrderForm/OrderForm";
+// import React from 'react';
+// import { TopBlock, Title, MainSection } from './app.styled';
+// import Container from './components/ui/Container';
+// import Cart from './components/Cart/Cart';
+// import Header from './components/Header/Header';
+// import ProductsList from './components/ProductsList';
+// import AuthForm from './components/Form/AuthForm';
+// import Search from './components/Search';
+// import OrderForm from './components/OrderForm/OrderForm';
 
-import debounce from "lodash.debounce";
+// import debounce from 'lodash.debounce';
 
-import products from "./assets/products";
+// import products from './assets/products';
 
-export default class App extends React.Component {
-  state = {
-    products,
-    cart: [],
-    searchQuery: "",
-    isCartModalOpen: false,
-    isAuthModalOpen: false,
-  };
+// export default class App extends React.Component {
+//   state = {
+//     products,
+//     cart: [],
+//     searchQuery: '',
+//     isCartModalOpen: false,
+//     isAuthModalOpen: false,
+//   };
 
-  addToCart = (productId) => {
-    const isProductInCart = this.state.cart.find(
-      (product) => product.id === productId
-    );
+//   addToCart = productId => {
+//     const isProductInCart = this.state.cart.find(
+//       product => product.id === productId
+//     );
 
-    if (!isProductInCart) {
-      const product = this.state.products.find(
-        (product) => product.id === productId
-      );
+//     if (!isProductInCart) {
+//       const product = this.state.products.find(
+//         product => product.id === productId
+//       );
 
-      this.setState((prevState) => ({
-        cart: [...prevState.cart, { ...product, quantity: 1 }],
-      }));
-    }
-  };
+//       this.setState(prevState => ({
+//         cart: [...prevState.cart, { ...product, quantity: 1 }],
+//       }));
+//     }
+//   };
 
-  handleIncrementProduct = (productId) => {
-    const product = this.state.cart.find((product) => product.id === productId);
+//   handleIncrementProduct = productId => {
+//     const product = this.state.cart.find(product => product.id === productId);
 
-    const updatedCart = this.state.cart.map((item) => {
-      if (product.id === item.id) {
-        return { ...item, quantity: (item.quantity += 1) };
-      }
-      return item;
-    });
+//     const updatedCart = this.state.cart.map(item => {
+//       if (product.id === item.id) {
+//         return { ...item, quantity: (item.quantity += 1) };
+//       }
+//       return item;
+//     });
 
-    this.setState({ cart: updatedCart });
-  };
+//     this.setState({ cart: updatedCart });
+//   };
 
-  handleDecrementProduct = (productId) => {
-    const product = this.state.cart.find((product) => product.id === productId);
+//   handleDecrementProduct = productId => {
+//     const product = this.state.cart.find(product => product.id === productId);
 
-    if (product.quantity <= 1) {
-      this.removeFromCart(productId);
-      return;
-    }
+//     if (product.quantity <= 1) {
+//       this.removeFromCart(productId);
+//       return;
+//     }
 
-    const updatedCart = this.state.cart.map((item) => {
-      if (product.id === item.id) {
-        return { ...item, quantity: (item.quantity -= 1) };
-      }
-      return item;
-    });
+//     const updatedCart = this.state.cart.map(item => {
+//       if (product.id === item.id) {
+//         return { ...item, quantity: (item.quantity -= 1) };
+//       }
+//       return item;
+//     });
 
-    this.setState({ cart: updatedCart });
-  };
+//     this.setState({ cart: updatedCart });
+//   };
 
-  removeFromCart = (productId) => {
-    const updatedCart = this.state.cart.filter(
-      (product) => product.id !== productId
-    );
-    this.setState({ cart: updatedCart });
-  };
+//   removeFromCart = productId => {
+//     const updatedCart = this.state.cart.filter(
+//       product => product.id !== productId
+//     );
+//     this.setState({ cart: updatedCart });
+//   };
 
-  getProductQuantity = (productId) => {
-    const product = this.state.cart.find((product) => product.id === productId);
-    return product?.quantity;
-  };
+//   getProductQuantity = productId => {
+//     const product = this.state.cart.find(product => product.id === productId);
+//     return product?.quantity;
+//   };
 
-  handleCartModal = () => {
-    this.setState((prevState) => ({
-      isCartModalOpen: !prevState.isCartModalOpen,
-    }));
-  };
+//   handleCartModal = () => {
+//     this.setState(prevState => ({
+//       isCartModalOpen: !prevState.isCartModalOpen,
+//     }));
+//   };
 
-  handleAuthModal = () => {
-    this.setState((prevState) => ({
-      isAuthModalOpen: !prevState.isAuthModalOpen,
-    }));
-  };
+//   handleAuthModal = () => {
+//     this.setState(prevState => ({
+//       isAuthModalOpen: !prevState.isAuthModalOpen,
+//     }));
+//   };
 
-  onSubmit = (data) => {
-    console.log(data);
-  };
+//   onSubmit = data => {
+//     console.log(data);
+//   };
 
-  changeSearchQuery = ({ target }) => {
-    this.setState({
-      searchQuery: target.value,
-    });
-  };
+//   changeSearchQuery = ({ target }) => {
+//     this.setState({
+//       searchQuery: target.value,
+//     });
+//   };
 
-  getProductsBySearchQuery = () => {
-    const normalizedSearchQuery = this.state.searchQuery.toLowerCase();
+//   getProductsBySearchQuery = () => {
+//     const normalizedSearchQuery = this.state.searchQuery.toLowerCase();
 
-    const filteredProducts = this.state.products.filter((product) =>
-      product.name.toLowerCase().includes(normalizedSearchQuery)
-    );
+//     const filteredProducts = this.state.products.filter(product =>
+//       product.name.toLowerCase().includes(normalizedSearchQuery)
+//     );
 
-    return filteredProducts;
-  };
+//     return filteredProducts;
+//   };
 
+//   render() {
+//     return (
+//       <Container>
+//         <Header
+//           cart={this.state.cart}
+//           handleCartModal={this.handleCartModal}
+//           handleAuthModal={this.handleAuthModal}
+//         />
+//         {/* <OrderForm onSubmit={this.onSubmit}/> */}
+//         <MainSection>
+//           <TopBlock>
+//             <Title>Products</Title>
+//             <Search
+//               value={this.state.searchQuery}
+//               onChange={this.changeSearchQuery}
+//             />
+//           </TopBlock>
+//           {this.getProductsBySearchQuery().length ? (
+//             <ProductsList
+//               products={this.getProductsBySearchQuery()}
+//               cart={this.state.cart}
+//               addToCart={this.addToCart}
+//             />
+//           ) : (
+//             <p>No matches found</p>
+//           )}
+//         </MainSection>
+
+//         {this.state.isCartModalOpen && (
+//           <Cart
+//             cart={this.state.cart}
+//             removeFromCart={this.removeFromCart}
+//             handleCartModal={this.handleCartModal}
+//             handleDecrementProduct={this.handleDecrementProduct}
+//             handleIncrementProduct={this.handleIncrementProduct}
+//           />
+//         )}
+//         {this.state.isAuthModalOpen && (
+//           <AuthForm
+//             onSubmit={this.onSubmit}
+//             handleAuthModal={this.handleAuthModal}
+//           />
+//         )}
+//       </Container>
+//     );
+//   }
+// }
+
+// !=========Repta=============
+
+import React, { Component } from 'react';
+import Counter from './components/Counter';
+import Dropdown from './components/Dropdown';
+import ColorPicker from './components/ColorPicker';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+class App extends Component {
   render() {
     return (
-      <Container>
-        <Header
-          cart={this.state.cart}
-          handleCartModal={this.handleCartModal}
-          handleAuthModal={this.handleAuthModal}
-        />
+      <>
+        <h1>Состояние компонента</h1>
 
-        {/* <OrderForm onSubmit={this.onSubmit}/> */}
-
-        <MainSection>
-          <TopBlock>
-            <Title>Products</Title>
-            <Search
-              value={this.state.searchQuery}
-              onChange={this.changeSearchQuery}
-            />
-          </TopBlock>
-          {this.getProductsBySearchQuery().length ? (
-            <ProductsList
-              products={this.getProductsBySearchQuery()}
-              cart={this.state.cart}
-              addToCart={this.addToCart}
-            />
-          ) : (
-            <p>No matches found</p>
-          )}
-        </MainSection>
-
-        {this.state.isCartModalOpen && (
-          <Cart
-            cart={this.state.cart}
-            removeFromCart={this.removeFromCart}
-            handleCartModal={this.handleCartModal}
-            handleDecrementProduct={this.handleDecrementProduct}
-            handleIncrementProduct={this.handleIncrementProduct}
-          />
-        )}
-
-        {this.state.isAuthModalOpen && (
-          <AuthForm
-            onSubmit={this.onSubmit}
-            handleAuthModal={this.handleAuthModal}
-          />
-        )}
-      </Container>
+        <Counter initialValue={10} />
+        <Dropdown />
+        <ColorPicker options={colorPickerOptions} />
+      </>
     );
   }
 }
+
+export default App;
