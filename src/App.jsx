5389c22bd0b014c7fdea3b3f4fe9a0efa97f9722
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-06-12 23:21:13 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@
 // import App from 'components/02-2-lesson/App';
 // import App from 'components/03-1-lifecycle/App';
 // import { App } from 'components/03-1-player/App';
-// import App from 'components/03-2-http-api/App';
+import App from 'components/03-2-http-api/App';
 // import { App } from 'components/03-3-http-api/App';
-import App from 'components/03-4-search-news/App';
+// import App from 'components/03-4-search-news/App';
 
 export const AppIndex = () => (
   <>
