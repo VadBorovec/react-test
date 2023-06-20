797c9dacc1febada4791635fb08c5cbbb039873d
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-06-20 22:54:12 +0300

    add hooks

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { AppIndex } from './App';
+// import { Global, ThemeProvider } from '@emotion/react';
+// import './index.css';
+// import { GlobalStyles, theme } from 'styles';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <ThemeProvider theme={theme}>
+//       <Global styles={GlobalStyles} />
+//       <AppIndex />
+//     </ThemeProvider>
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { AppIndex } from './App';
-import { Global, ThemeProvider } from '@emotion/react';
+import AuthProvider from './components/04-1-hooks/contexts/auth/Provider';
+// import 'modern-normalize/modern-normalize.css';
 import './index.css';
-import { GlobalStyles, theme } from 'styles';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyles} />
-      <AppIndex />
-    </ThemeProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <AppIndex />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
