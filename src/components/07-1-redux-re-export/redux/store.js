cdc4b3cee9262fbfedab4fe5ff10c6168a1b057f
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-07-07 18:00:25 +0300

    delete duplicate

--- src/components/07-1-redux-re-export/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import booksReducer from './books/booksReducer';
-
-export const store = configureStore({
-  reducer: {
-    books: booksReducer,
-  },
-});
