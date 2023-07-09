91e5da3a0c5e336ff2788c9d67b1b966ae9336ed
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-07-09 16:01:14 +0300

    Update App.jsx

--- src/components/07-hw/components/App/App.jsx
+++ src/components/07-hw/components/App/App.jsx
@@ -32,31 +32,3 @@ export const App1 = () => {
     </Container>
   );
 };
-
-// !====
-// // import { lazy } from 'react';
-// import { Toaster } from 'react-hot-toast';
-// import { Routes, Route } from 'react-router-dom';
-// import { Layout } from '../Layout';
-
-// import { CreateTodoPage, HomePage, TodosPage, NotFoundView } from '../../pages';
-
-// // const { HomePage, TodosPage, CreateTodoPage, NotFoundView } = lazy(() =>
-// //   import('../../pages')
-// // );
-
-// export const App1 = () => {
-//   return (
-//     <>
-//       <Routes>
-//         <Route path="/" element={<Layout />}>
-//           <Route index element={<HomePage />} />
-//           <Route path="todos" element={<TodosPage />} />
-//           <Route path="create" element={<CreateTodoPage />} />
-//           <Route element={<NotFoundView />} />
-//         </Route>
-//       </Routes>
-//       <Toaster position="top-right" />
-//     </>
-//   );
-// };
